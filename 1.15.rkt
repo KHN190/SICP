3285e6bbf111af01e61d7c690cc635deea89530f
SalamanderMinds <shanjian1984@gmail.com>
2013-11-10 16:06:08 +0800

    update

---
+++ 1.15.rkt
@@ -0,0 +1,11 @@
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+;;; a) (sine 12.15) calls 'p' 5 times.
+;;; b) O(log(a)).
